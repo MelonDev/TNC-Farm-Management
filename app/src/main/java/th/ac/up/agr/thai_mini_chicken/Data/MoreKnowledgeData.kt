69b823b6b8ffe409ac562cd1e41e90b00740309b
Melondev <melondev@icloud.com>
2019-11-29 15:40:51 +0700

    Rename TableData to MoreKnowledgeData

--- app/src/main/java/th/ac/up/agr/thai_mini_chicken/Data/MoreKnowledgeData.kt
+++ app/src/main/java/th/ac/up/agr/thai_mini_chicken/Data/MoreKnowledgeData.kt
@@ -1,9 +1,10 @@
 package th.ac.up.agr.thai_mini_chicken.Data
 
 
-data class TableData(
+data class MoreKnowledgeData(
         var title_card: String = "",
         var manual_card: String = "",
         var type_card: String = "",
         var image: Int = 0
 )
+
