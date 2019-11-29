bf33c836cf4d95536ff64cfc8a143ef3d680b6e1
Melondev <melondev@icloud.com>
2019-11-29 11:25:07 +0700

    Changing normal parcelable to @Parcelize

--- app/src/main/java/th/ac/up/agr/thai_mini_chicken/Data/CardData.kt
+++ app/src/main/java/th/ac/up/agr/thai_mini_chicken/Data/CardData.kt
@@ -1,9 +1,9 @@
 package th.ac.up.agr.thai_mini_chicken.Data
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
-
+@Parcelize
 data class CardData(
         var dateDay: String = "",
         var dateMonth: String = "",
@@ -25,63 +25,4 @@ data class CardData(
         var notiBefore: String = "",
         var managerName: String = "",
         var managerObjective: String = ""
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "")
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(dateDay)
-        parcel.writeString(dateMonth)
-        parcel.writeString(dateYear)
-        parcel.writeString(amountMale)
-        parcel.writeString(amountFemale)
-        parcel.writeString(systemFarm)
-        parcel.writeString(userObjective)
-        parcel.writeString(status)
-        parcel.writeString(createDate)
-        parcel.writeString(lastUpdate)
-        parcel.writeString(moveToHistory)
-        parcel.writeString(cardName)
-        parcel.writeString(cardID)
-        parcel.writeString(breed)
-        parcel.writeString(ageDay)
-        parcel.writeString(ageWeek)
-        parcel.writeString(notification)
-        parcel.writeString(notiBefore)
-        parcel.writeString(managerName)
-        parcel.writeString(managerObjective)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<CardData> {
-        override fun createFromParcel(parcel: Parcel): CardData {
-            return CardData(parcel)
-        }
-
-        override fun newArray(size: Int): Array<CardData?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
