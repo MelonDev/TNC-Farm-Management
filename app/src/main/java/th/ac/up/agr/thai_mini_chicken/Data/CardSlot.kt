d109445d8be0742f7bf00a6dda20e7eda23baa2a
Melondev <melondev@icloud.com>
2019-11-29 18:30:32 +0700

    Add duration to CardSlot

--- app/src/main/java/th/ac/up/agr/thai_mini_chicken/Data/CardSlot.kt
+++ app/src/main/java/th/ac/up/agr/thai_mini_chicken/Data/CardSlot.kt
@@ -3,5 +3,6 @@ package th.ac.up.agr.thai_mini_chicken.Data
 data class CardSlot(
         var dataCard: CardData = CardData(),
         var event: Event = Event(),
-        var day: String = ""
+        var day: String = "",
+        var duration: Int = 0
 )
